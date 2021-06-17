75214157d449bdf1fddf7efb60122b71fa1f8982
asher8346 <asher.schulman@gmail.com>
2021-06-16 20:24:23 -0500

    refactored CRUD api operations to use try/catch

--- routes/api/tasks.js
+++ routes/api/tasks.js
@@ -5,41 +5,60 @@ const router = express.Router()
 const Task = require('../../models/Task.model')
 
 // GET /api/tasks/
-router.get('/', (req, res) => {
-    Task.find()
-        .then(tasks => res.json(tasks))
+router.get('/', async (req, res) => {
+    try {
+        res.status(200).json(
+            await Task.find({})
+        )
+    } catch (err) {
+        res.status(400).json(err)
+    }
 })
-
 // POST /api/tasks/
-router.post('/', (req, res) => {
-    const newTask = new Task({
-        taskId: req.body.taskId,
-        title: req.body.title,
-        description: req.body.description,
-        categories: req.body.categories,
-        createdBy: req.body.parentWorkspaceID,
-    });
-    newTask.save().then(task => res.json(task))
+router.post('/', async (req, res) => {
+    try {
+        const newTask = new Task({
+            taskId: req.body.taskId,
+            title: req.body.title,
+            description: req.body.description,
+            categories: req.body.categories,
+            createdBy: req.body.parentWorkspaceID,
+        })
+        newTask.save()
+            .then(task => res.status(200).json(task))
+    } catch (err) {
+        res.status(400).json(err)
+    }
 })
-
-// PUT /api/tasks/:id
-router.put('/:id', (req, res) => {
-    Task.findByIdAndUpdate(req.params.id)
-    .then()
-})
-
 // GET /api/tasks/:id
-router.get('/:id', (req, res) => {
-    Task.findById(req.params.id)
-    .then( task => res.json(task))
-    .catch( err => res.status(404).json({success: false}))
+router.get('/:id', async (req, res) => {
+    try {
+        res.status(200).json(
+            await Task.findById(req.params.id)
+        )
+    } catch (err) {
+        res.status(400).json(err)
+    }
 })
-
+// PUT /api/tasks/:id
+router.put('/:id', async (req, res) => {
+    try {
+        res.status(200).json(
+            await Task.findByIdAndUpdate(req.params.id, req.body, { new: true })
+        )
+    } catch (err) {
+        res.status(400).json(err)
+    }
+})
 // DELETE /api/tasks/:id
-router.delete('/:id', (req, res) => {
-    Task.findById(req.params.id)
-    .then( task => task.remove().then(() => res.json({success: true})))
-    .catch( err => res.status(404).json({success: false}))
+router.delete('/:id', async (req, res) => {
+    try {
+        res.status(200).json(
+            await Task.findByIdAndRemove(req.params.id)
+        )
+    } catch (err) {
+        res.status(400).json(err)
+    }
 })
 
 module.exports = router;
