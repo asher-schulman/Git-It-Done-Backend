8cde29d629ca822a30533b90915bef5322fd060f
Asher Schulman <asher.schulman@gmail.com>
2023-01-21 17:52:33 -0600

    changing database connection; migration from Heroku -> Cyclic

--- server.js
+++ server.js
@@ -1,40 +1,35 @@
 /////////////////////////
 // DEPENDENCIES
 /////////////////////////
-require('dotenv').config()
-const express = require('express')
-const cors = require ('cors')
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 // const mongoose = require('mongoose')
 const port = process.env.PORT || 8000;
-const database = require('./database')
+const database = require("./database");
 
-const taskRouter = require('./routes/api/tasks')
-const userRouter = require('./routes/api/users')
-const workspaceRouter = require('./routes/api/workspaces.js')
+const taskRouter = require("./routes/api/tasks");
+const userRouter = require("./routes/api/users");
+const workspaceRouter = require("./routes/api/workspaces.js");
 
 /////////////////////////
 // The Application Object
 /////////////////////////
-const app = express()
-
-/////////////////////////
-// Database
-/////////////////////////
-database.connectDB()
+const app = express();
 
 /////////////////////////
 // MIDDLEWARE
 /////////////////////////
-app.use(cors({ origin: true, credentials: true }))
-app.use(express.json())
+app.use(cors({ origin: true, credentials: true }));
+app.use(express.json());
 
 /////////////////////////
 // Routers
 /////////////////////////
 // url endpoints for us to communicate with our API
-app.use('/api/tasks', taskRouter)
+app.use("/api/tasks", taskRouter);
 // app.use('/api/users', userRouter)
-app.use('/api/workspaces', workspaceRouter)
+app.use("/api/workspaces", workspaceRouter);
 
 /////////////////////////
 // API Endpoints
@@ -42,30 +37,30 @@ app.use('/api/workspaces', workspaceRouter)
 // database like .findById() CRUD operations
 // test route that sends some json data to make sure server is working
 app.get("/", async (req, res) => {
-    try {
-        res.status(200).json( await {
-            response: "Hello World",
-            key: 'value pair!'
-        })
-    } catch (err) {
-        res.status(400).json(err)
-    }
-})
+	try {
+		res.redirect("/api");
+	} catch (err) {
+		res.status(400).json(err);
+	}
+});
 
 app.get("/api", async (req, res) => {
-    try {
-        res.status(200).json( await {
-            response: "Welcome to Git It Done's API",
-            instructions: "Use the routes below to get specific data. This API will have 3 routers, and includes full CRUD operations as well as robust database queries. All api URLs take in :id params at the end as well as additional parameters. See the raw code in /routes/api for more URLs and parameters",
-            users: "NOT CURRENTLY FUNCTIONAL. /api/users - returns an array of all users",
-            workspaces: "/api/workspaces - returns as array of all workspaces",
-            tasks: "/api/tasks - returns as array of all tasks",
-            tasksInSpecificWorkspace: "/api/workspaces/:id/alltasks"
-        })
-    } catch (err) {
-        res.status(400).json(err)
-    }
-})
+	try {
+		res.status(200).json(
+			await {
+				response: "Welcome to Git It Done's API",
+				instructions:
+					"Use the routes below to get specific data. This API will have 3 routers, and includes full CRUD operations as well as robust database queries. All api URLs take in :id params at the end as well as additional parameters. See the raw code in /routes/api for more URLs and parameters",
+				users: "NOT CURRENTLY FUNCTIONAL. /api/users - returns an array of all users",
+				workspaces: "/api/workspaces - returns as array of all workspaces",
+				tasks: "/api/tasks - returns as array of all tasks",
+				tasksInSpecificWorkspace: "/api/workspaces/:id/alltasks",
+			}
+		);
+	} catch (err) {
+		res.status(400).json(err);
+	}
+});
 
 // used to sync database info with frontend
 // app.get('/sync', (req, res) => {
@@ -74,13 +69,20 @@ app.get("/api", async (req, res) => {
 //             res.status(500).send(err)
 //         } else {
 //             res.status(200).send(data)
-//         } 
+//         }
 //     })
 // })
 
-/////////////////////////
-// Listener
-/////////////////////////
-app.listen(port, () => {
-  console.log(`the backend server is running on port ${port}`);
-});
+function databaseConnection() {
+	/////////////////////////
+	// Database
+	/////////////////////////
+	database.connectDB();
+	/////////////////////////
+	// Listener
+	/////////////////////////
+	app.listen(port, () => {
+		console.log(`the backend server is running on port ${port}`);
+	});
+}
+databaseConnection();
